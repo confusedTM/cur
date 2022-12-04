4cd6eda7cb4bbd671e08152fde7f02bf71d80cdc
olegg <olegg@LAPTOP-N6G66DVI>
2022-12-04 19:34:43 +0400

    Добавьте файлы проекта.

---
+++ cur/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace cur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.layoutMatrix = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.layoutVectorB = new System.Windows.Forms.TableLayoutPanel();
+            this.layoutVectorX = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.25428F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.57049F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.04348F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.layoutMatrix, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.layoutVectorB, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.layoutVectorX, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(759, 422);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.numericUpDown1);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 326);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(300, 45);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Уравнений:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.numericUpDown1.Location = new System.Drawing.Point(102, 9);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(86, 27);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.Location = new System.Drawing.Point(196, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(682, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Корни";
+            // 
+            // layoutMatrix
+            // 
+            this.layoutMatrix.BackColor = System.Drawing.Color.Silver;
+            this.layoutMatrix.ColumnCount = 1;
+            this.layoutMatrix.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutMatrix.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutMatrix.Location = new System.Drawing.Point(4, 25);
+            this.layoutMatrix.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.layoutMatrix.Name = "layoutMatrix";
+            this.layoutMatrix.RowCount = 1;
+            this.layoutMatrix.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutMatrix.Size = new System.Drawing.Size(548, 291);
+            this.layoutMatrix.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Коэффициенты";
+            // 
+            // layoutVectorB
+            // 
+            this.layoutVectorB.BackColor = System.Drawing.Color.Silver;
+            this.layoutVectorB.ColumnCount = 1;
+            this.layoutVectorB.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.layoutVectorB.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.layoutVectorB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutVectorB.Location = new System.Drawing.Point(559, 24);
+            this.layoutVectorB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.layoutVectorB.Name = "layoutVectorB";
+            this.layoutVectorB.RowCount = 1;
+            this.layoutVectorB.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.layoutVectorB.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 293F));
+            this.layoutVectorB.Size = new System.Drawing.Size(97, 293);
+            this.layoutVectorB.TabIndex = 6;
+            // 
+            // layoutVectorX
+            // 
+            this.layoutVectorX.BackColor = System.Drawing.Color.Silver;
+            this.layoutVectorX.ColumnCount = 1;
+            this.layoutVectorX.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutVectorX.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutVectorX.Location = new System.Drawing.Point(662, 24);
+            this.layoutVectorX.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.layoutVectorX.Name = "layoutVectorX";
+            this.layoutVectorX.RowCount = 1;
+            this.layoutVectorX.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutVectorX.Size = new System.Drawing.Size(94, 293);
+            this.layoutVectorX.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(563, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Свободные";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.button2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 379);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(301, 40);
+            this.flowLayoutPanel2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 33);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Максимизировать след";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 422);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel layoutMatrix;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel layoutVectorB;
+        private System.Windows.Forms.TableLayoutPanel layoutVectorX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ cur/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cur
+{
+    public partial class Form1 : Form
+    {
+
+        private int n;
+        private TextBox[,] matr;
+        private TextBox[] vectorB;
+        private TextBox[] vectorX;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string DefaultText = String.Format("{0:f}", 0.0);
+
+        // создаёт текстбокс
+        private TextBox InitTextBox(bool readOnly)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            textBox.Text = DefaultText;
+            textBox.ReadOnly = readOnly;
+            if (!readOnly)
+            {
+                textBox.CausesValidation = true;
+                textBox.Validating += ValidateTextBox;
+            }
+            return textBox;
+        }
+
+        // проверяет правильность текста в текстбоксе
+        private void ValidateTextBox(object sender, CancelEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            double result;
+            e.Cancel = !double.TryParse(textBox.Text, out result);
+        }
+
+        // создаёт вдумерный массив текстбоксов, вставляя в layout
+        private TextBox[,] InitTextBoxMatrix(TableLayoutPanel layoutPanel, int count, bool readOnly)
+        {
+            layoutPanel.SuspendLayout();
+
+            layoutPanel.Controls.Clear();
+
+            layoutPanel.ColumnStyles.Clear();
+            layoutPanel.ColumnCount = count;
+
+            layoutPanel.RowStyles.Clear();
+            layoutPanel.RowCount = count;
+
+            TextBox[,] result = new TextBox[count, count];
+            float cellSize = 1f / count * 100f;
+
+            for (int col = 0; col < count; col++)
+            {
+                layoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, cellSize));
+                for (int row = 0; row < count; row++)
+                {
+                    layoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, cellSize));
+
+                    TextBox textBox = InitTextBox(readOnly);
+
+                    layoutPanel.Controls.Add(textBox, col, row);
+                    result[col, row] = textBox;
+                }
+            }
+
+            layoutPanel.ResumeLayout(true);
+
+            return result;
+        }
+
+        // создаёт одномерный массив текстарий, вставляя в layout
+        private TextBox[] InitTextBoxArray(TableLayoutPanel layoutPanel, int count, bool readOnly)
+        {
+            layoutPanel.SuspendLayout();
+
+            layoutPanel.Controls.Clear();
+
+            layoutPanel.ColumnStyles.Clear();
+            layoutPanel.ColumnCount = 1;
+
+            layoutPanel.RowStyles.Clear();
+            layoutPanel.RowCount = count;
+
+            TextBox[] result = new TextBox[count];
+            float cellSize = 1f / count * 100f;
+
+            layoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f));
+
+            for (int row = 0; row < count; row++)
+            {
+                layoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, cellSize));
+
+                TextBox textBox = InitTextBox(readOnly);
+
+                layoutPanel.Controls.Add(textBox, 0, row);
+                result[row] = textBox;
+            }
+
+            layoutPanel.ResumeLayout(true);
+
+            return result;
+        }
+
+        private void InitMatrix()
+        {
+            matr = InitTextBoxMatrix(layoutMatrix, n, false);
+        }
+
+        private void InitVectorX()
+        {
+            vectorX = InitTextBoxArray(layoutVectorX, n, true);
+        }
+
+        private void InitVectorB()
+        {
+            vectorB = InitTextBoxArray(layoutVectorB, n, false);
+        }
+
+        public int N
+        {
+            get { return n; }
+            set
+            {
+                if (value != n && value > 0)
+                {
+                    n = value;
+                    InitMatrix();
+                    InitVectorX();
+                    InitVectorB();
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            N = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            N = (int)numericUpDown1.Value;
+        }
+
+        public double[][] Matrix
+        {
+            get
+            {
+                // собираем введённую пользователем матрицу A
+                double[][] matrix = new double[n][];
+                for (int i = 0; i < n; i++)
+                {
+                    matrix[i] = new double[n];
+                    for (int j = 0; j < n; j++)
+                        matrix[i][j] = double.Parse(matr[j, i].Text);
+                }
+                return matrix;
+            }
+            set
+            {
+                // записываем в текстбоксы матрицу A
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        matr[j,i].Text = value[i][j].ToString("f");
+            }
+        }
+
+        public double[] VectorB
+        {
+            get
+            {
+                // собираем введённый пользователем вектор B
+                double[] vector_b = new double[n];
+                for (int j = 0; j < n; j++)
+                    vector_b[j] = double.Parse(vectorB[j].Text);
+                return vector_b;
+            }
+            set
+            {
+                // записываем в текстбоксы вектор B
+                for (int j = 0; j < n; j++)
+                    vectorB[j].Text = value[j].ToString("f");
+            }
+        }
+
+        public double[] VectorX
+        {
+            set
+            {
+                // показываем вычисленный результат X
+                for (int j = 0; j < n; j++)
+                    vectorX[n-j-1].Text = value[j].ToString("f");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[][] mat = Matrix;
+            double[] b = VectorB;
+
+            double[][] maxmat = (double[][])Matrix.Clone();
+            double[] maxb = (double[])VectorB.Clone();
+            double max = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                max += mat[i][i];
+            }
+
+            bool[] chosen = new bool[n];
+            LinkedList<double[]> permutationMat = new LinkedList<double[]>();
+            LinkedList<double> permutationB = new LinkedList<double>();
+            search(chosen, permutationMat, permutationB, mat, b, max, maxmat, maxb);
+            Matrix = maxmat;
+            VectorB = maxb;
+        }
+
+        private void search(bool[] chosen, LinkedList<double[]> permutationMat, LinkedList<double> permutationB, double[][] mat, double[] b, double MaxSumOfElementsOfMainDiagonal, double[][] maxmat, double[] maxb)
+        {
+            if (permutationMat.Count == n)
+            {
+                // обработка перестановки
+                double g = CountSumOfElementsOfMainDiagonal(permutationMat);
+                if (g > MaxSumOfElementsOfMainDiagonal)
+                {
+                    MaxSumOfElementsOfMainDiagonal = g;
+                    for (int i = 0; i < n; i++)
+                    {
+                        maxmat[i] = permutationMat.ElementAt(i);
+                        maxb[i] = permutationB.ElementAt(i);
+                    }
+                }
+            }
+            else
+            {
+                // генератор перестановок
+                for (int i = 0; i < n; i++)
+                {
+                    if (chosen[i]) continue;
+                    chosen[i] = true;
+                    permutationMat.AddLast(mat[i]);
+                    permutationB.AddLast(b[i]);
+                    search(chosen, permutationMat, permutationB, mat, b, MaxSumOfElementsOfMainDiagonal, maxmat, maxb);
+                    chosen[i] = false;
+                    permutationMat.RemoveLast();
+                    permutationB.RemoveLast();
+                }
+            }
+        }
+        // считатет сумму элементов главной диагонали
+        private double CountSumOfElementsOfMainDiagonal(LinkedList<double[]> permutation)
+        {
+            double sl = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sl += permutation.ElementAt(i)[i];
+            }
+
+            return sl;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VectorX = Gauss(VectorB, Matrix);
+        }
+
+        //метод Гаусса
+        private double[] Gauss(double[] free, double[][] matrix)
+        {
+
+            double[][] MatrixCopy = (double[][])matrix.Clone();   //копирование матрицы
+            double[] FreeCopy = (double[])free.Clone();     //копирование вектора свободных членов
+
+            for (int i = 0; i < n - 1; i++)
+            {       //приведение к верхнему треугольному виду
+                for (int j = i + 1; j < n; j++)
+                {
+                    double koef = MatrixCopy[j][i] / MatrixCopy[i][i];
+                    for (int k = i; k < n; k++)
+                    {
+                        MatrixCopy[j][k] -= MatrixCopy[i][k] * koef;
+                    }
+                    FreeCopy[j] -= FreeCopy[i] * koef;        //изменение вектора свободных членов
+                }
+            }
+
+            int count = 1;
+            double[] x = new double[n];
+            x[0] = FreeCopy[n - 1] / MatrixCopy[n - 1][n - 1]; // вычисление n-ой неизвестной
+            //обртаный ход
+            for (int i = 1, k = n - 2; i < n && k >= 0; i++, k--)
+            {       
+                for (int j = n - 1; j != k; j--, count++)
+                {
+                    x[i] = (FreeCopy[k] + MatrixCopy[k][j] * (-1) * x[i - count]) / MatrixCopy[k][k];
+                }
+                count = 1;
+            }
+
+            return x;
+        }
+    }
+}
